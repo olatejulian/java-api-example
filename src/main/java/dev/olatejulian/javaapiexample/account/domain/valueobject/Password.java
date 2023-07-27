eedee8cec235e88b74682c740b8beb57f8669c10
olatejulian <julian.lf.olate@gmail.com>
2023-07-27 16:09:49 -0300

    refactor: :recycle: rewrite already hashed password construction

--- src/main/java/dev/olatejulian/javaapiexample/account/domain/valueobject/Password.java
+++ src/main/java/dev/olatejulian/javaapiexample/account/domain/valueobject/Password.java
@@ -1,34 +1,42 @@
 package dev.olatejulian.javaapiexample.account.domain.valueobject;
 
+import java.util.regex.Pattern;
+
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import dev.olatejulian.javaapiexample.account.domain.exception.InvalidPasswordException;
 import lombok.Getter;
 
 public class Password {
+    private static final Integer PASSWORD_MIN_LENGTH = 8;
+
+    private static final Integer PASSWORD_MAX_LENGTH = 255;
+
     @Getter
     private String password;
 
-    public Password(String password, Boolean hashed) throws InvalidPasswordException {
-        if (hashed) {
-            this.password = password;
-        } else {
-            validate(password);
+    public Password(String password) throws InvalidPasswordException {
+        validate(password);
 
-            this.password = hash(password);
+        this.password = hash(password);
+    }
+
+    private Password(HashedPassword hashedPassword) {
+        this.password = hashedPassword.getPassword();
+    }
+
+    public static Password fromHashedString(String password) throws InvalidPasswordException {
+        var hashedPassword = HashedPassword.of(password);
+
+        if (hashedPassword == null) {
+            throw new InvalidPasswordException("Hash does not match bcrypt regex");
         }
-    }
 
-    private static Integer passwordMinLength() {
-        return 8;
-    }
-
-    private static Integer passwordMaxLength() {
-        return 255;
+        return new Password(hashedPassword);
     }
 
     private static void validate(String password) throws InvalidPasswordException {
-        if (password.length() < passwordMinLength() || password.length() > passwordMaxLength()) {
+        if (password.length() < PASSWORD_MIN_LENGTH || password.length() > PASSWORD_MAX_LENGTH) {
             throw new InvalidPasswordException("Invalid password length");
         }
     }
@@ -43,5 +51,34 @@ public class Password {
         var passwordEncoder = new BCryptPasswordEncoder();
 
         return passwordEncoder.matches(rawPassword, this.password);
+    }
+}
+
+class HashedPassword {
+    private static final String BCRYPT_REGEX = "^\\$2[aby]\\$[0-9]{2}\\$[./0-9A-Za-z]{53}$";
+
+    @Getter
+    private String password;
+
+    private HashedPassword(String password) {
+        this.password = password;
+    }
+
+    public static HashedPassword of(String password) {
+        HashedPassword hashedPassword = null;
+
+        if (validate(password)) {
+            hashedPassword = new HashedPassword(password);
+        }
+
+        return hashedPassword;
+    }
+
+    private static boolean validate(String password) {
+        var pattern = Pattern.compile(BCRYPT_REGEX);
+
+        var matcher = pattern.matcher(password);
+
+        return matcher.matches();
     }
 }
