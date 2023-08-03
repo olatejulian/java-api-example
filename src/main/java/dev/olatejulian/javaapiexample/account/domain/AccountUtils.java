6d059f6beb8a3a21d8f8b51016df175951df0890
olatejulian <julian.lf.olate@gmail.com>
2023-08-02 21:30:34 -0300

    refactor: :recycle: update account utils to new password domain rule

--- src/main/java/dev/olatejulian/javaapiexample/account/domain/AccountUtils.java
+++ src/main/java/dev/olatejulian/javaapiexample/account/domain/AccountUtils.java
@@ -7,21 +7,27 @@ import dev.olatejulian.javaapiexample.account.domain.valueobject.Password;
 import dev.olatejulian.javaapiexample.shared.domain.valueobject.EmailAddress;
 
 public class AccountUtils {
-    private AccountUtils() {
-    }
-
     public static final Account createRandomAccount() {
+        var randomName = RandomStringUtils.randomAlphabetic(10);
+
+        var randomEmailAddress = RandomStringUtils.randomAlphabetic(10).concat("@email.com");
+
+        var randomPassword = RandomStringUtils.randomAlphanumeric(10) + "1@aA";
+
         try {
-            var name = new AccountName(RandomStringUtils.randomAlphabetic(10));
+            var name = new AccountName(randomName);
 
-            var emailAddress = new EmailAddress(RandomStringUtils.randomAlphabetic(10).concat("@email.com"));
+            var emailAddress = new EmailAddress(randomEmailAddress);
 
-            var password = new Password(RandomStringUtils.randomAlphanumeric(10));
+            var password = new Password(randomPassword);
 
             return Account.create(name, emailAddress, password);
 
         } catch (Exception e) {
             return null;
         }
+    }
+
+    private AccountUtils() {
     }
 }
