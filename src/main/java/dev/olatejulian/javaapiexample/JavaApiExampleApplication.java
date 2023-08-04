c58fe33cc95badb7bd49ad31332be671b4154ea8
olatejulian <julian.lf.olate@gmail.com>
2023-08-03 22:20:19 -0300

    feat: :sparkles: add spring boot application bootstrap

---
+++ src/main/java/dev/olatejulian/javaapiexample/JavaApiExampleApplication.java
@@ -0,0 +1,12 @@
+package dev.olatejulian.javaapiexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaApiExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaApiExampleApplication.class, args);
+	}
+}
