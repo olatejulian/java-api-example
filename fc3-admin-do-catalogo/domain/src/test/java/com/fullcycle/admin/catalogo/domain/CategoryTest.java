314476328eec73d54a1b9e7dd2c9f17d59b990a0
olatejulian <julian.lf.olate@gmail.com>
2024-05-12 23:23:17 -0300

    add example package

---
+++ fc3-admin-do-catalogo/domain/src/test/java/com/fullcycle/admin/catalogo/domain/CategoryTest.java
@@ -0,0 +1,11 @@
+package com.fullcycle.admin.catalogo.domain;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+class CategoryTest {
+    @Test
+    public void testCategory {
+        Assertions.assertNotNull(new Category());
+    };
+}
